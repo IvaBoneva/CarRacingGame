60d6f60e7bf52f100a8a853cc2abb517e254f9f9
Iva-Kamenova <iva.kamenova2004@abv.bg>
2025-07-04 19:57:14 +0300

    Now we have our road and our car - we can move it.

--- CarRacingGame/Form1.Designer.cs
+++ CarRacingGame/Form1.Designer.cs
@@ -28,12 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            line1 = new PictureBox();
+            line2 = new PictureBox();
+            line3 = new PictureBox();
+            line4 = new PictureBox();
+            line5 = new PictureBox();
+            line6 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            car = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)line1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)line2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)line3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)line4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)line5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)line6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)car).BeginInit();
+            SuspendLayout();
+            // 
+            // line1
+            // 
+            line1.BackColor = Color.White;
+            line1.Location = new Point(185, -1);
+            line1.Name = "line1";
+            line1.Size = new Size(12, 95);
+            line1.TabIndex = 0;
+            line1.TabStop = false;
+            // 
+            // line2
+            // 
+            line2.BackColor = Color.White;
+            line2.Location = new Point(185, 115);
+            line2.Name = "line2";
+            line2.Size = new Size(12, 95);
+            line2.TabIndex = 1;
+            line2.TabStop = false;
+            // 
+            // line3
+            // 
+            line3.BackColor = Color.White;
+            line3.Location = new Point(185, 241);
+            line3.Name = "line3";
+            line3.Size = new Size(12, 95);
+            line3.TabIndex = 2;
+            line3.TabStop = false;
+            // 
+            // line4
+            // 
+            line4.BackColor = Color.White;
+            line4.Location = new Point(185, 372);
+            line4.Name = "line4";
+            line4.Size = new Size(12, 95);
+            line4.TabIndex = 3;
+            line4.TabStop = false;
+            // 
+            // line5
+            // 
+            line5.BackColor = Color.White;
+            line5.Location = new Point(-1, -5);
+            line5.Name = "line5";
+            line5.Size = new Size(10, 472);
+            line5.TabIndex = 4;
+            line5.TabStop = false;
+            // 
+            // line6
+            // 
+            line6.BackColor = Color.White;
+            line6.Location = new Point(374, -5);
+            line6.Name = "line6";
+            line6.Size = new Size(10, 472);
+            line6.TabIndex = 5;
+            line6.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // car
+            // 
+            car.Image = (Image)resources.GetObject("car.Image");
+            car.Location = new Point(46, 281);
+            car.Name = "car";
+            car.Size = new Size(84, 78);
+            car.SizeMode = PictureBoxSizeMode.StretchImage;
+            car.TabIndex = 6;
+            car.TabStop = false;
+            car.Click += car_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gray;
+            ClientSize = new Size(382, 453);
+            Controls.Add(car);
+            Controls.Add(line6);
+            Controls.Add(line5);
+            Controls.Add(line4);
+            Controls.Add(line3);
+            Controls.Add(line2);
+            Controls.Add(line1);
+            Name = "Form1";
+            Text = "Car Racing Game";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)line1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)line2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)line3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)line4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)line5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)line6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)car).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox line1;
+        private PictureBox line2;
+        private PictureBox line3;
+        private PictureBox line4;
+        private PictureBox line5;
+        private PictureBox line6;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox car;
     }
 }
--- CarRacingGame/Form1.cs
+++ CarRacingGame/Form1.cs
@@ -6,5 +6,44 @@ namespace CarRacingGame
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveLines(5);
+        }
+
+        void moveLines(int speed)
+        {
+            PictureBox[] lines = { line1, line2, line3, line4, line5, line6 };
+
+
+            for (int i = 0; i < lines.Length - 2; i++)
+            {
+                if (lines[i].Top >= 500) lines[i].Top = 0;
+                else lines[i].Top += speed;
+            }
+        }
+
+        private void car_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                car.Left += -8;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                car.Left += 8;
+            }
+        }
     }
 }
